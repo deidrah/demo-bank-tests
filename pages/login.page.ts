271b3dcfb6f62475f80960f256469b3904d70e7a
Deidrah <dominikastasik93@gmail.com>
2023-11-28 19:12:19 +0100

    Update login.page.ts

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -5,7 +5,10 @@ export class LoginPage {
     constructor(private page: Page) {}
 
 
-    loginInput = this.page.getByTestId('login-input')
-    passwordInput = this.page.getByTestId('password-input')
-    loginButton = this.page.getByTestId('login-button')
+    loginInput = this.page.getByTestId('login-input');
+    passwordInput = this.page.getByTestId('password-input');
+    loginButton = this.page.getByTestId('login-button');
+
+    loginError = this.page.getByTestId('error-login-id');
+  passwordError = this.page.getByTestId('error-login-password');
 }
