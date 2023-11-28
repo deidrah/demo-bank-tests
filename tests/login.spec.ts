257a1a1cdf66e691930deacda9530b3ba58bbd5e
Deidrah <dominikastasik93@gmail.com>
2023-11-28 18:17:08 +0100

    refactor + another tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,16 +1,19 @@
 import { test, expect } from '@playwright/test';
-
+import { loginData } from './test-data/login.data';
 
 test.describe('User login to Demobank', () => {
+  test.beforeEach(async ({ page }) => {
+    const url = '/';
+    await page.goto(url);
+  });
+
   test('successful login with correct credentials', async ({ page }) => {
     // Arrange
-    const url = 'https://demo-bank.vercel.app/';
-    const userId = 'testerLO';
-    const userPassword = '10987654';
+    const userId = loginData.userId;
+    const userPassword = loginData.userPassword;
     const expectedUserName = 'Jan Demobankowy';
 
     // Act
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
@@ -21,13 +24,11 @@ test.describe('User login to Demobank', () => {
   
   test('unsuccessful login with too short password', async ({ page }) => {
     // Arrange
-    const url = 'https://demo-bank.vercel.app/';
-    const userId = 'testerLO';
+    const userId = loginData.userId;
     const incorrectPassword = '1234';
     const expectedErrorMessage = 'hasło ma min. 8 znaków';
   
     // Act
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').fill(incorrectPassword);
     await page.getByTestId('password-input').blur();
